b354e5c6a9c905157d3a4b1a016954142539db53
Carlos Interone <carlos@mostlyclicks.com>
2014-04-22 08:27:09 -0500

    start format of projects

--- vendor/extensions/projects/app/controllers/refinery/projects/projects_controller.rb
+++ vendor/extensions/projects/app/controllers/refinery/projects/projects_controller.rb
@@ -9,6 +9,7 @@ module Refinery
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @project in the line below:
         present(@page)
+        @categories = Refinery::Categories::Category.all
       end
 
       def show
