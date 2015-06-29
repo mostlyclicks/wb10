87351e1d81867454495f4d9b89f5dfa118421bf9
Carlos Interone <carlos@mostlyclicks.com>
2015-06-29 13:58:24 -0500

    add project testimonials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :get_categories
+  before_filter :get_categories, :get_testimonials
 
   protected
 
     def get_categories
       @categories = Refinery::Categories::Category.all
     end
+
+    def get_testimonials
+      @projects_all = Refinery::Projects::Project.all
+      @testimonials = []
+      @projects_all.each do |t|
+        if t.testimonial?
+          @testimonials << t
+        end
+      end
+      @testimonial_sample = @testimonials.sample
+    end
+
+
 end
--- vendor/extensions/projects/refinerycms-projects.gemspec
+++ vendor/extensions/projects/refinerycms-projects.gemspec
@@ -3,6 +3,7 @@
 Gem::Specification.new do |s|
   s.platform          = Gem::Platform::RUBY
   s.name              = 'refinerycms-projects'
+  s.author            = 'mc'
   s.version           = '1.0'
   s.description       = 'Ruby on Rails Projects extension for Refinery CMS'
   s.date              = '2014-04-21'
