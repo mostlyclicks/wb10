a0fd2b4d21e0f412557b1cf5fb60547af7774a9e
Carlos Interone <carlos@mostlyclicks.com>
2015-12-02 09:04:26 -0600

    fix navs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,20 @@ module ApplicationHelper
     presenter
   end
 
+  def child_sub_menu_pages_mobile
+    #This works on parent and sub pages -- use 'select' instead of 'detect'
+    sub_menu_items = Refinery::Menu.new(refinery_menu_pages.select{ |item| item.parent_id == @page.root.id })
+    # style = page_style(@page)
+    presenter = Refinery::Pages::MenuPresenter.new(sub_menu_items, self)
+    presenter.menu_tag = "div class=\"collapse3 navbar-collapse3 \" id=\"sub-nav-collapse\""
+    # presenter.css = "#{style}"
+    presenter.list_tag = "ul class=\"nav nav-pills3  \" style=\"margin-top:-60px;\""
+    # presenter.list_tag_css = "nav nav-pills nav-stacked"
+    presenter.max_depth = 1
+    presenter.selected_css = "active"
+    first_css = :first
+    last_css = :last
+    presenter
+  end
+
 end
